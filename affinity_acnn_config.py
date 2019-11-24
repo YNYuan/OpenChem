40d7f4a65408e99cbdfff7f9d396f2a57961f346
YNYuan <xmyyn@126.com>
2019-11-24 14:58:07 -0500

    using minus energy

--- affinity_acnn_config.py
+++ affinity_acnn_config.py
@@ -37,13 +37,21 @@ def my_target_split(whole_path, train_idx, test_idx):
 
 
 class GraphDataset(Dataset):
-    def __init__(self, X_matrix, Z_matrix, R_matrix, Nbrs_Z_matrix, target):
+    def __init__(self, X_matrix_c, Z_matrix_c, R_matrix_c, Nbrs_Z_matrix_c, X_matrix_l, Z_matrix_l, R_matrix_l, Nbrs_Z_matrix_l, X_matrix_r, Z_matrix_r, R_matrix_r, Nbrs_Z_matrix_r, target):
         super(GraphDataset, self).__init__()
-        self.X_matrix = X_matrix
-        self.Z_matrix = Z_matrix
+        self.X_matrix_c = X_matrix_c
+        self.Z_matrix_c = Z_matrix_c
         #self.Nbrs_matrix = Nbrs_matrix
-        self.R_matrix = R_matrix
-        self.Nbrs_Z_matrix = Nbrs_Z_matrix
+        self.R_matrix_c = R_matrix_c
+        self.Nbrs_Z_matrix_c = Nbrs_Z_matrix_c
+        self.X_matrix_l = X_matrix_l
+        self.Z_matrix_l = Z_matrix_l
+        self.R_matrix_l = R_matrix_l
+        self.Nbrs_Z_matrix_l = Nbrs_Z_matrix_l
+        self.X_matrix_r = X_matrix_r
+        self.Z_matrix_r = Z_matrix_r
+        self.R_matrix_r = R_matrix_r
+        self.Nbrs_Z_matrix_r = Nbrs_Z_matrix_r
         self.target = target
         #self.num_features = X_matrix.shape[2]
 
@@ -52,29 +60,47 @@ class GraphDataset(Dataset):
 
     def __getitem__(self, index):
         sample = {
-            'X_matrix': self.X_matrix[index].astype('float32'),
-            'Z_matrix': self.Z_matrix[index].astype('float32'),
+            'X_matrix_c': self.X_matrix_c[index].astype('float32'),
+            'Z_matrix_c': self.Z_matrix_c[index].astype('float32'),
             # 'Nbrs_matrix': self.Nbrs_matrix[index].astype('float32'),
-            'R_matrix': self.R_matrix[index].astype('float32'),
-            'Nbrs_Z_matrix': self.Nbrs_Z_matrix[index].astype('float32'),
+            'R_matrix_c': self.R_matrix_c[index].astype('float32'),
+            'Nbrs_Z_matrix_c': self.Nbrs_Z_matrix_c[index].astype('float32'),
+            'X_matrix_l': self.X_matrix_l[index].astype('float32'),
+            'Z_matrix_l': self.Z_matrix_l[index].astype('float32'),
+            'R_matrix_l': self.R_matrix_l[index].astype('float32'),
+            'Nbrs_Z_matrix_l': self.Nbrs_Z_matrix_l[index].astype('float32'),
+            'X_matrix_r': self.X_matrix_r[index].astype('float32'),
+            'Z_matrix_r': self.Z_matrix_r[index].astype('float32'),
+            'R_matrix_r': self.R_matrix_r[index].astype('float32'),
+            'Nbrs_Z_matrix_r': self.Nbrs_Z_matrix_r[index].astype('float32'),
             'target': self.target[index].astype('float32')
         }
         return sample
 
-def get_dataset(xpath, zpath, nbrspath, nbrszpath, targetpath):
-    train_idx, test_idx = random_index(2770, 0.8)
+def get_dataset(xpath_c, zpath_c, nbrspath_c, nbrszpath_c, xpath_l, zpath_l, nbrspath_l, nbrszpath_l, xpath_r, zpath_r, nbrspath_r, nbrszpath_r, targetpath):
+    train_idx, test_idx = random_index(277, 0.8)
 
-    trainX, testX = my_feature_split(xpath, train_idx, test_idx)
-    trainZ, testZ = my_feature_split(zpath, train_idx, test_idx)
-    trainNbrs, testNbrs = my_feature_split(nbrspath, train_idx, test_idx)
-    trainNbrs_Z, testNbrs_Z = my_feature_split(nbrszpath, train_idx, test_idx)
+    trainX_c, testX_c = my_feature_split(xpath_c, train_idx, test_idx)
+    trainZ_c, testZ_c = my_feature_split(zpath_c, train_idx, test_idx)
+    trainNbrs_c, testNbrs_c = my_feature_split(nbrspath_c, train_idx, test_idx)
+    trainNbrs_Z_c, testNbrs_Z_c = my_feature_split(nbrszpath_c, train_idx, test_idx)
+    trainX_l, testX_l = my_feature_split(xpath_l, train_idx, test_idx)
+    trainZ_l, testZ_l = my_feature_split(zpath_l, train_idx, test_idx)
+    trainNbrs_l, testNbrs_l = my_feature_split(nbrspath_l, train_idx, test_idx)
+    trainNbrs_Z_l, testNbrs_Z_l = my_feature_split(nbrszpath_l, train_idx, test_idx)
+    trainX_r, testX_r = my_feature_split(xpath_r, train_idx, test_idx)
+    trainZ_r, testZ_r = my_feature_split(zpath_r, train_idx, test_idx)
+    trainNbrs_r, testNbrs_r = my_feature_split(nbrspath_c, train_idx, test_idx)
+    trainNbrs_Z_r, testNbrs_Z_r = my_feature_split(nbrszpath_r, train_idx, test_idx)
     trainY, testY = my_target_split(targetpath, train_idx, test_idx)
-    train_dataset = GraphDataset(trainX, trainZ, trainNbrs, trainNbrs_Z, trainY)
-    test_dataset = GraphDataset(testX, testZ, testNbrs, testNbrs_Z, testY)
+    train_dataset = GraphDataset(trainX_c, trainZ_c, trainNbrs_c, trainNbrs_Z_c, trainX_l, trainZ_l, trainNbrs_l, trainNbrs_Z_l, trainX_r, trainZ_r, trainNbrs_r, trainNbrs_Z_r, trainY)
+    test_dataset = GraphDataset(testX_c, testZ_c, testNbrs_c, testNbrs_Z_c, testX_l, testZ_l, testNbrs_l, testNbrs_Z_l, testX_r, testZ_r, testNbrs_r, testNbrs_Z_r, testY)
     return train_dataset, test_dataset
 
 train_dataset, test_dataset = get_dataset('../3d_dataset/complex_matrix.npy', '../3d_dataset/complex_type.npy', 
-'../3d_dataset/complex_distance_matrix.npy', '../3d_dataset/complex_atomtype_matrix.npy',
+'../3d_dataset/complex_distance_matrix.npy', '../3d_dataset/complex_atomtype_matrix.npy', '../3d_dataset/ligand_matrix.npy', '../3d_dataset/ligand_type.npy', 
+'../3d_dataset/ligand_distance_matrix.npy', '../3d_dataset/ligand_atomtype_matrix.npy', '../3d_dataset/rec_matrix.npy', '../3d_dataset/rec_type.npy', 
+'../3d_dataset/rec_distance_matrix.npy', '../3d_dataset/rec_atomtype_matrix.npy'
 '../3d_dataset/whole_data.csv')
 
 model = AtomicConvModel0
@@ -83,10 +109,10 @@ model_params = {
     'task': 'regression',
     'data_layer': GraphDataset,
     'use_clip_grad': False,
-    'batch_size': 3,
-    'num_epochs': 3,
+    'batch_size': 2,
+    'num_epochs': 10,
     'logdir': './acnnlogs',
-    'print_every': 10,
+    'print_every': 1,
     'save_every': 1,
     'train_data_layer': train_dataset,
     'val_data_layer': test_dataset,
