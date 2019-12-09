1135cf6d37f606f7e6123e2b06496d18b897b639
YNYuan <xmyyn@126.com>
2019-12-08 21:44:45 -0500

    faster

--- affinity_acnn_config.py
+++ affinity_acnn_config.py
@@ -91,7 +91,7 @@ model_params = {
     'task': 'regression',
     'data_layer': GraphDataset_Multi,
     'use_clip_grad': False,
-    'batch_size': 12,
+    'batch_size': 24,
     'num_epochs': 100,
     'logdir': './acnnlogs',
     'print_every': 10,
@@ -102,7 +102,7 @@ model_params = {
     'criterion': nn.MSELoss(),
     'optimizer': Adam,
     'optimizer_params': {
-        'lr': 0.005,
+        'lr': 0.02,
     },
     'lr_scheduler': StepLR,
     'lr_scheduler_params': {
